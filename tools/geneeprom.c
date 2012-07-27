bdc440f46c5e51a915749e940bc80a6aca10dcce
Grzegorz Daniluk <grzegorz.daniluk@gmail.com>
2012-07-27 16:27:55 +0200

    greg_tmp_mess: updated tlv generation tool for eeprom init script and sfp db

--- tools/geneeprom.c
+++ tools/geneeprom.c
@@ -13,12 +13,15 @@
 #define END_SFPSEC "##END_SFP_SECTION"
 #define START_INITSEC "##INIT_SECTION"
 #define END_INITSEC "##END_INIT_SECTION"
-#define SFP_TLV "eesfp.tlv"
-#define INIT_TLV "eeinit.tlv"
+#define TLV_FILE "wrc.tlv"
+#define SECTION_DONE 0
+#define SECTION_START 1
+#define SECTION_FLUSH 2
+#define TLV_HDR_LEN 5
 
 
 int line_empty(char *line, unsigned int len);
-int create_sfp_tlv(struct s_sfpinfo *sfps, int len);
+int create_tlv(struct s_sfpinfo *sfps, int sfplen, char *initbuf, int buflen, uint8_t is_end);
 int32_t swap_endian(int32_t a);
 int sfp_extract(char *line, unsigned int len, struct s_sfpinfo *sfp);
 
@@ -26,9 +29,8 @@ int main(int argc, char **argv)
 {
   FILE *file;
   char *buf=NULL;
-  //char buf[SFPS_MAX][80];
   struct s_sfpinfo sfps[SFPS_MAX];
-  uint8_t p_sfp=0, p_init=0, c_sfp=0, c_init=0;
+  uint8_t p_sfp=0, p_init=0;
   int idx=0, size, buflen;
 
   if( argc<2 )
@@ -51,14 +53,12 @@ int main(int argc, char **argv)
     buflen = getline(&buf, &size, file);
 
     if( line_empty(buf, buflen) ) continue;
-    buf[buflen-1] = '\0'; //drop \n at the end of the line
-    printf("gotline=%ssize=%u\n", buf, buflen);
-    if(!strncmp(buf, START_SFPSEC, buflen))       { p_sfp=1; continue; }
-    else if(!strncmp(buf, END_SFPSEC, buflen))    { p_sfp=0; continue; }
-    else if(!strncmp(buf, START_INITSEC, buflen)) { p_init=1; continue; }
-    else if(!strncmp(buf, END_INITSEC, buflen))   { p_init=0; continue; }
+    if(!strncmp(buf, START_SFPSEC, buflen-2))       { p_sfp=SECTION_START; continue; }
+    else if(!strncmp(buf, END_SFPSEC, buflen-2))    { p_sfp=SECTION_FLUSH; }
+    else if(!strncmp(buf, START_INITSEC, buflen-2)) { p_init=SECTION_START; continue; }
+    else if(!strncmp(buf, END_INITSEC, buflen-2))   { p_init=SECTION_FLUSH; }
 
-    if(p_sfp==1 && p_init==1)
+    if(p_sfp==SECTION_START && p_init==SECTION_START)
     {
       fprintf(stderr, "Syntax error in input file!!!\n");
       fclose(file);
@@ -66,9 +66,9 @@ int main(int argc, char **argv)
       return -1;
     }
 
-    if(p_sfp)
+    if(p_sfp==SECTION_START)
     {
-      c_sfp = 1;
+      buf[buflen-1] = '\0'; //drop \n at the end of the line
       sfp_extract(buf, strlen(buf), &sfps[idx++]);
       if(idx>=SFPS_MAX) 
       {
@@ -78,19 +78,31 @@ int main(int argc, char **argv)
         return -1;
       }
     }
-    else if(p_init)
+    else if(p_sfp==SECTION_FLUSH)
     {
-      c_init = 1;
+      create_tlv(sfps, idx, NULL, 0, 0);
+      p_sfp=SECTION_DONE;
+    }
+    else if(p_init==SECTION_START)
+    {
+      if(create_tlv(NULL, 0, buf, buflen, 0) < 0)
+      {
+        fclose(file);
+        free(buf);
+        return -1;
+      }
+    }
+    else if(p_init==SECTION_FLUSH)
+    {
+      create_tlv(NULL, 0, NULL, 0, 1);
+      p_init=SECTION_DONE;
     }
 
   }
+  fclose(file);
+  free(buf);
 
-  //check which file to create (or both?)
-  if(c_sfp)
-  {
-    create_sfp_tlv(sfps, idx);
-  }
-
+  printf("TLV file created\n");
   return 0;
 }
 
@@ -137,7 +149,7 @@ int line_empty(char *line, unsigned int len)
 {
   int i;
 
-  if(line==NULL)
+  if(line==NULL || len==-1)
     return 1;
 
   for(i=0; i<len; ++i)
@@ -149,38 +161,86 @@ int line_empty(char *line, unsigned int len)
   return 1;
 }
 
-int create_sfp_tlv(struct s_sfpinfo *sfps, int len)
-{
-  int fd, i;
-  uint32_t tmpbuf;
 
-  fd = open(SFP_TLV, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-  if(fd==-1)
+int create_tlv(struct s_sfpinfo *sfps, int sfplen, char *initbuf, int buflen, uint8_t is_end)
+{
+  static int fd;
+  int i;
+  uint32_t tmpbuf;
+  static uint32_t init_tlv_base=0;
+  static uint8_t f_open=1, f_init=1;
+  static uint16_t init_sz=0;
+
+  if(f_open)
   {
-    perror("Could not write to file");
-    return -1;
+    fd = open(TLV_FILE, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    if(fd==-1)
+    {
+      perror("Could not write to sfp file");
+      return -1;
+    }
+    f_open = 0;
   }
 
-  //tlv write operation
-  tmpbuf = 'w';
-  write(fd, &tmpbuf, 1);
-  //tlv base address
-  tmpbuf = EE_BASE_SFP;
-  write(fd, &tmpbuf, 2);
-  //tlv section size
-  tmpbuf = len*sizeof(struct s_sfpinfo) + 1; //+1 for the count(1B) -> see dev/eeprom.c
-  write(fd, &tmpbuf, 2);
-  //count(1B)
-  write(fd, &len, 1);
-  for(i=0; i<len; ++i)
+  if(sfplen>0)
   {
-    sfps[i].dTx = swap_endian(sfps[i].dTx);
-    sfps[i].dRx = swap_endian(sfps[i].dRx);
-    sfps[i].alpha = swap_endian(sfps[i].alpha);
-    write(fd, (uint8_t*)&sfps[i], sizeof(struct s_sfpinfo) );
-  }  
+    if(!f_init) //the init script was writen earlier
+    {
+      lseek(fd, init_tlv_base, SEEK_SET);
+      tmpbuf = 'w';
+      write(fd, &tmpbuf, 1);
+      tmpbuf = EE_BASE_INIT;
+      write(fd, &tmpbuf, 2);
+      tmpbuf = init_sz + 2; //+2 for _bytes used_
+      write(fd, &tmpbuf, 2);
+      write(fd, &init_sz, 2);
+    }
+    //tlv write operation
+    tmpbuf = 'w';
+    write(fd, &tmpbuf, 1);
+    //tlv base address
+    tmpbuf = EE_BASE_SFP;
+    write(fd, &tmpbuf, 2);
+    //tlv section size
+    tmpbuf = sfplen*sizeof(struct s_sfpinfo) + 1; //+1 for the count(1B) -> see dev/eeprom.c
+    init_tlv_base = tmpbuf + TLV_HDR_LEN; 
+    write(fd, &tmpbuf, 2);
+    //count(1B)
+    write(fd, &sfplen, 1);
+    for(i=0; i<sfplen; ++i)
+    {
+      sfps[i].dTx = swap_endian(sfps[i].dTx);
+      sfps[i].dRx = swap_endian(sfps[i].dRx);
+      sfps[i].alpha = swap_endian(sfps[i].alpha);
+      write(fd, (uint8_t*)&sfps[i], sizeof(struct s_sfpinfo) );
+    }  
+  } 
+  else if(buflen>0)
+  {
+    if(f_init) lseek(fd, init_tlv_base+7, SEEK_SET);
+    f_init = 0;
 
-  close(fd);
+    if(buflen)
+    {
+      printf("init cmd\n");
+      write(fd, initbuf, buflen);
+      init_sz += buflen;
+    }
+  }
+
+  if(is_end && !f_init) 
+  {
+    lseek(fd, init_tlv_base, SEEK_SET);
+    tmpbuf = 'w';
+    write(fd, &tmpbuf, 1);
+    tmpbuf = EE_BASE_INIT;
+    write(fd, &tmpbuf, 2);
+    tmpbuf = init_sz + 2; //+2 for _bytes used_
+    write(fd, &tmpbuf, 2);
+    tmpbuf = (init_sz&0xff00)>>8 | (init_sz&0x00ff)<<8; //swap endianness
+    write(fd, &tmpbuf, 2);
+  }
+  if(is_end) close(fd);
 
   return 0;
 }
