9635fc39c3c059a9610e7ec6446d26b3149779ca
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2013-01-10 11:09:34 +0100

    ppsi_monitor.c: restored "mode: WR Slave / Master" indication
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- wrc_ptp_ppsi.c
+++ wrc_ptp_ppsi.c
@@ -21,8 +21,8 @@
 #include "pps_gen.h"
 #include "uart.h"
 
-static int ptp_enabled = 0, ptp_mode = WRC_MODE_UNKNOWN,
-	ptp_forced_stop = 0;
+int ptp_mode = WRC_MODE_UNKNOWN;
+static int ptp_enabled = 0, ptp_forced_stop = 0;
 struct pp_instance ppi_static; /* FIXME: no more static, because used in
 				   tests/measure_t24p.c */
 
